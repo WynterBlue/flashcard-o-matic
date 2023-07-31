9c468536f13056aea6044248870699480163a240
Alex <wynterblue02@gmail.com>
2023-07-31 13:55:16 -0400

    Home menu code functional

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,14 +1,37 @@
 import React from "react";
 import Header from "./Header";
+import Home from "./Home";
+import CreateDeck from "./CreateDeck";
+import ViewDeck from "./ViewDeck";
+import StudyScreen from "./StudyScreen";
 import NotFound from "./NotFound";
+import { Switch, Router, Link, Route } from "react-router-dom/cjs/react-router-dom";
 
 function Layout() {
   return (
     <>
       <Header />
       <div className="container">
+        {/*routes go here */
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/decks/new">
+            <CreateDeck />
+          </Route>
+          <Route exact path="/decks/:deckId">
+            <ViewDeck />
+          </Route>
+          <Route path="/decks/:deckId/study">
+            <StudyScreen />
+          </Route>
+          <Route>
+            <NotFound />  
+          </Route>
+        </Switch>
+        }
         {/* TODO: Implement the screen starting here */}
-        <NotFound />
       </div>
     </>
   );
